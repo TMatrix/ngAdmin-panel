66d8b6760107f342a9984cd9114350713d65357f
Mark Prykhoda <hroscko@gmail.com>
2020-07-05 19:39:43 +0300

    Created shared components

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from './../../modules/dashboard/dashboard.component';
+import { PostsComponent } from './../../modules/posts/posts.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [DefaultComponent, DashboardComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [DefaultComponent, DashboardComponent, PostsComponent],
+  imports: [CommonModule, RouterModule, SharedModule],
 })
 export class DefaultModule {}
